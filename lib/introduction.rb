efacad4b63e6981674f996b29abed03b6ab6e820
Chad Johnson <chad@chadjohn.com>
2021-02-24 03:33:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{{name} and I am learning to programin #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
